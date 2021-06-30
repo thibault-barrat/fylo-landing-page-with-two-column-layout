78d9542cc62d394c4961f750c503722b5952982a
thibault-barrat <contact@thibault-barrat.com>
2021-06-30 15:35:00 +0200

    add error message in the form

---
+++ dist/js/main.js
@@ -0,0 +1,26 @@
+function ValidateEmail(input) {
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (input.value.match(validRegex)) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+document.querySelectorAll('.signin-form').forEach(item => {
+  item.addEventListener('submit', (event) => {
+    var form = event.target;
+    if(!ValidateEmail(form.querySelector('.signin-form__input'))) {
+      event.preventDefault();
+      form.querySelector('.signin-form__error').style.display = 'block';
+      form.querySelector('.signin-form__input').style.borderColor = 'red';
+      return false;
+    } else {
+      form.querySelector('.signin-form__error').style.display = 'none';
+      form.querySelector('.signin-form__input').style.borderColor = '#070439';
+    }
+  })
+})
+
